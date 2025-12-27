f698e4dafbe7f9044f35dc137b69e18ff8fb2770
ReyRodriP <reynaldorodriguez802@gmail.com>
2025-12-27 12:45:49 -0400

    Mejoras visuales a la tabla y agregado de funcion publicar

--- frontend/JS/script.js
+++ frontend/JS/script.js
@@ -32,11 +32,25 @@ function showInfo(response) {
         const credits = document.createElement('td')
         credits.innerText = a.creditos
 
+        const grades = document.createElement('td')
+        grades.innerText = a.calificacion 
+        if(grades.innerText.trim() !== '') row.classList.add('finished') //Para cambiar estilo al tener nota
+
         //Seccion de opciones
         const options = document.createElement('td')
         const button1 = document.createElement('button')
+        button1.classList.add('bx', 'bx-pencil-sparkles')
+        button1.title = "Inscribir"
+
         const button2 = document.createElement('button')
+        button2.classList.add('bx', 'bx-seal-check')
+        button2.title = `Publicar`
+        button2.addEventListener('click', () => { showGrades(a.id)}) 
+
         const button3 = document.createElement('button')
+        button3.classList.add('bx', 'bx-rotate-ccw')  
+        button3.title = "Reiniciar"
+
         options.appendChild(button1)
         options.appendChild(button2)
         options.appendChild(button3)
@@ -47,9 +61,24 @@ function showInfo(response) {
         row.appendChild(teoricHours)
         row.appendChild(practicHours)
         row.appendChild(credits)
+        row.appendChild(grades)
         row.append(options)
-
 
         subjects.appendChild(row)
     })     
+}
+
+async function showGrades(id) {
+    let cal = prompt('Ingresa la calificacion final')
+    if(!cal) return;
+
+    await fetch(apiUrl+`/${id}`, {
+        method: "PUT", 
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "completada": 1, "calificacion": `${parseFloat(cal)}`})
+    })
+    .then(res => res.json())
+    .then(response => console.log(response)) //Llamada a la api para el PUT
+
+    location.reload()
 }

b5e0f8817c2b902de70caea0854131363da7a6ab
ReyRodriP <reynaldorodriguez802@gmail.com>
2026-01-08 20:20:02 -0400

    Login y registro funcionando

---
+++ frontend/JS/login.js
@@ -0,0 +1,26 @@
+const apiUrl = "http://localhost:3000/login"
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const loginBtn = document.getElementById("loginBtn")
+
+const log = async (event) => {
+    event.preventDefault()
+    if(password.value.length < 5) return alert('Contraseña invalida') //Poner mas validaciones y mejorar estilo a futuro
+
+    const res = await fetch(apiUrl, option = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ "correo": email.value, "contrasena": password.value}),
+        credentials: "include" 
+    })
+
+     if (!res.ok) {
+      alert(data.Mensaje || "Error al iniciar sesión");
+      return;
+    }
+    
+    window.location.href = 'index.html'
+
+}
+
+loginBtn.addEventListener('click', log)
--- frontend/JS/register.js
+++ frontend/JS/register.js
@@ -4,16 +4,28 @@ const email = document.getElementById('email')
 const password = document.getElementById('password')
 const register = document.getElementById('register')
 const apiUrl = "http://localhost:3000/register"
-const options = {
-    method: "POST"
+
+const registerStudent = async (event) => { 
+    event.preventDefault() //Para evitar que recargue
+
+    //Validaciones. Recuerda ponerlo bonito a futuro 
+    if(userName.value.length < 3) return alert('El nombre de usuario debe tener 3 caracteres minimos') 
+    if(studentCode.value.length != 9) return alert('Matricula invalida') 
+    if(password.value.length < 5) return alert('Contraseña demasiado corta') 
+
+    await fetch(apiUrl, options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            "nombre": userName.value, 
+            "matricula": parseFloat(studentCode.value), 
+            "correo": email.value, 
+            "contrasena": password.value})
+    })
+    .then(res => res.json())
+    .then(response => response)
+
+    window.location.href = 'login.html'
 }
 
-fetch(apiUrl, options)
-.then(res => res.json())
-.then(response => console.log(response))
-
-const login = () => {
-    console.log(userName.value)
-}
-
-register.addEventListener('click', login)
+register.addEventListener('click', registerStudent)
